9fe02760071b0fad31f58d63696b1d86c6c9523d
Cory Spitzer & Kevin Chen <student@epicodus.com>
2015-04-23 16:06:43 -0700

    create anagram method

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,4 +2,15 @@ class String
   define_method(:anagram_of?) do |word|
     word.split('').sort() == self.split('').sort()
   end
+
+  define_method(:anagrams) do |words|
+    compare_word = self
+    words.each_with_index() do |word, index|
+      if compare_word.anagram_of?(word) == false
+        words.pop(index)
+      end
+    end
+    words
+  end
+
 end
