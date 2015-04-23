9d307781cc2a391651f3a68cc8b9822d56a398a7
Cory Spitzer & Kevin Chen <student@epicodus.com>
2015-04-23 15:28:27 -0700

    Fix typo in anagram_of? test.

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -4,7 +4,7 @@ require('anagram')
 describe('String#anagram_of?') do
   # tells us if a word is an anagram
   it('returns true if a word is an anagram') do
-    expect('act'.anagram_of('cat')).to(eq(true))
+    expect('act'.anagram_of?('cat')).to(eq(true))
   end
 
 end
