674ff0bfdaa160a9bc70140285a2c22009bed0bf
Cory Spitzer & Kevin Chen <student@epicodus.com>
2015-04-23 15:20:15 -0700

    Test if 'act' is an anagram of 'cat'.

---
+++ spec/anagram_spec.rb
@@ -0,0 +1,10 @@
+require('rspec')
+require('anagram')
+
+describe('String#anagram_of?') do
+  # tells us if a word is an anagram
+  it('returns true if a word is an anagram') do
+    expect('act'.anagram_of('cat')).to(eq(true))
+  end
+
+end
